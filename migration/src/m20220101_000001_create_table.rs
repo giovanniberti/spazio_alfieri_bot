6eb2c55d363f8abc413be998295e3c2d118b4ce1
Giovanni Berti <dev.giovanniberti@gmail.com>
2024-09-29 17:59:46 +0200

    feat: add tables to be created in initial migration

--- migration/src/m20220101_000001_create_table.rs
+++ migration/src/m20220101_000001_create_table.rs
@@ -6,36 +6,96 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
+        manager
+            .create_table(
+                Table::create()
+                    .table(Newsletter::Table)
+                    .if_not_exists()
+                    .col(pk_auto(Newsletter::Id))
+                    .col(string_uniq(Newsletter::Link))
+                    .col(integer_null(Newsletter::MessageId))
+                    .to_owned(),
+            )
+            .await?;
 
         manager
             .create_table(
                 Table::create()
-                    .table(Post::Table)
+                    .table(Program::Table)
                     .if_not_exists()
-                    .col(pk_auto(Post::Id))
-                    .col(string(Post::Title))
-                    .col(string(Post::Text))
+                    .col(pk_auto(Program::Id))
+                    .col(integer(Program::NewsletterId))
+                    .col(string(Program::Title))
+                    .foreign_key(
+                        ForeignKey::create()
+                            .name("fk_program_newsletter")
+                            .from(Program::Table, Program::NewsletterId)
+                            .to(Newsletter::Table, Newsletter::Id)
+                            .on_delete(ForeignKeyAction::Cascade)
+                            .on_update(ForeignKeyAction::Cascade),
+                    )
+                    .to_owned(),
+            )
+            .await?;
+
+        manager
+            .create_table(
+                Table::create()
+                    .table(Entry::Table)
+                    .if_not_exists()
+                    .col(pk_auto(Entry::Id))
+                    .col(integer(Entry::ProgramId))
+                    .col(timestamp_with_time_zone(Entry::Date))
+                    .col(string_null(Entry::Details))
+                    .foreign_key(
+                        ForeignKey::create()
+                            .name("fk_entry_program")
+                            .from(Entry::Table, Entry::ProgramId)
+                            .to(Program::Table, Program::Id)
+                            .on_delete(ForeignKeyAction::Cascade)
+                            .on_update(ForeignKeyAction::Cascade),
+                    )
                     .to_owned(),
             )
             .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
+        manager
+            .drop_table(Table::drop().table(Entry::Table).to_owned())
+            .await?;
 
         manager
-            .drop_table(Table::drop().table(Post::Table).to_owned())
+            .drop_table(Table::drop().table(Program::Table).to_owned())
+            .await?;
+
+        manager
+            .drop_table(Table::drop().table(Newsletter::Table).to_owned())
             .await
     }
 }
 
 #[derive(DeriveIden)]
-enum Post {
+enum Newsletter {
     Table,
     Id,
+    Link,
+    MessageId,
+}
+
+#[derive(DeriveIden)]
+enum Program {
+    Table,
+    Id,
+    NewsletterId,
     Title,
-    Text,
+}
+
+#[derive(DeriveIden)]
+enum Entry {
+    Table,
+    Id,
+    ProgramId,
+    Date,
+    Details,
 }
